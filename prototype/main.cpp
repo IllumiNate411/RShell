dff4aebece070195c0367823738117f374b3604b
IllumiNate411 <IllumiNate411@gmail.com>
2020-01-29 20:39:49 -0800

    Prototype for fork, waitpid and execvp completed

---
+++ prototype/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	cout << "Executed Successfully!" << endl;
+	
+	return 0;
+}
--- prototype/test.cpp
+++ prototype/test.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
 using namespace std;
 
 int main(int argc, char** argv) {
 	
+	pid_t cPID, wPID;
+	int status;
+
+	cPID = fork();
+
+	if (cPID < 0) {
+		cout << "Failed to Fork to child correctly" << endl;
+	}
+	else if (cPID == 0) {
+		if (execvp("./main", NULL) < 0) {
+			cout << "Execute Failed" << endl;
+		}
+	}
+	else {
+		wPID = waitpid(cPID, &status, 0);
+		if (wPID == -1) {
+			cout << "Wait PID failed" << endl;
+		}
+		
+		else if (WIFSIGNALED(status)) {
+			cout << "Error exiting" << endl;
+		}
+		
+		else {
+			cout << "Successfully exited" << endl;
+		}
+	}
+
+	return 0;
 }
