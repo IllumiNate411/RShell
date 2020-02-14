e87f9bee2a0b55c3d198f77dad2c187a3728f7c0
Steven-Joseph <sjose007@ucr.edu>
2020-02-13 23:58:56 -0800

    done

---
+++ unit_test/Or_Tests.hpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+#include "../header/Or.hpp"
+
+
+//TEST(ConnectorTestSet, OrTest_1) {
+//	const char** ex[3]
+//	const char** exx[3]
+//	ex[0] = "echo";
+//	ex[1] = "hello";
+//	ex[2] = '\0'
+//	exx[0] = "echo";
+//	exx[1] = "world";
+//	exx[2] = '\0'
+//	executable* ex1 = new expression*[ex];
+//	executable* ex2 = new expression*[exx];
+//	executable* or1 = new Or*(ex1,ex2)
+//	EXPECT_EQ(or1->execute,true);
+//
+//}
+
